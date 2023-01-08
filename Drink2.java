987543ec787379e51b269ef3218b75b9e18c2cc1
ktakada42 <ktakada950711@gmail.com>
2023-01-08 23:16:46 +0900

    feat: chapter 5

---
+++ Drink2.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Drink2 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("飲み物は何が好きですか?");
+            System.out.println("a オレンジジュース");
+            System.out.println("b コーヒー");
+            System.out.println("c どちらでもない");
+            String line;
+            while (true) {
+                System.out.println("a, b, cのどれかを選んでください。");
+                line = reader.readLine();
+                if (line.length() == 0) {
+                } else {
+                    break ;
+                }
+            }
+            char c = line.charAt(0);
+            switch (c) {
+                case 'a':
+                    System.out.println("オレンジジュースです。");
+                    break ;
+
+                case 'b':
+                    System.out.println("コーヒーです。");
+                    break ;
+
+                default:
+                    System.out.println("どちらでもありません。");
+                    break ;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ DrinkSwitch.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class DrinkSwitch {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("飲み物は何が好きですか?");
+            System.out.println("あ=オレンジジュース");
+            System.out.println("い=コーヒー");
+            System.out.println("う=どちらでもない");
+            System.out.println("あ, い, う, のどれかを選んでください。");
+            String line = reader.readLine();
+            switch (line) {
+                case "あ":
+                    System.out.println("オレンジジュースです。");
+                    break ;
+
+                case "い":
+                    System.out.println("コーヒーです。");
+                    break ;
+
+                default:
+                    System.out.println("どちらでもありません。");
+                    break ;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
