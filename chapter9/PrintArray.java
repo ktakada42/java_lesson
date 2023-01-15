2b7150b6a6027bd370d8f6b71a604a425ff86b73
ktakada42 <ktakada950711@gmail.com>
2023-01-15 23:58:38 +0900

    feat: chapter 9

---
+++ chapter9/PrintArray.java
@@ -0,0 +1,22 @@
+public class PrintArray {
+	public static void main(String[] args) {
+		int[][] arr = {
+			{ 3, 1, 4, 1, },
+			{ 5, 9, 2, },
+			{ 6, 5, },
+			{ 3, },
+		};
+		printArray(arr);
+	}
+	public static void printArray(int[][] arr) {
+		System.out.println("{");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("\t{");
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(" " + arr[i][j] + ",");
+			}
+			System.out.println(" },");
+		}
+		System.out.println("}");
+	}
+}	
